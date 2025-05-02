24a6df664fbc97b47f2fa6b6ce05f631c955a1c9
deepeshyadavrd <raoshsbh6@gmail.com>
2025-05-02 13:24:22 +0530

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ImageSlider from '../components/ImageSlider';
 import CategoryList from '../components/CategoryList';
-
+import SearchPage from '../components/SearchPage';
 
 export default function Home() {
+  const [searchQuery, setSearchQuery] = useState('');
   return (
     <>
     {/* <Navbar /> */}
     <ImageSlider />
     {/* <SearchBar /> */}
     <CategoryList />
-  
+  <div>
+      <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      {searchQuery.trim() !== '' && <SearchPage query={searchQuery} />}
+    </div>
     </>
+    
   );
 }
